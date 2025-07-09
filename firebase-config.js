2e3cbce1d291b0e95b4cd13ecf5309b5995f38b2
ARCHI GOYAL <144723374+archiiigoyal@users.noreply.github.com>
2025-07-10 02:46:09 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAjJUS8izi_JfQpWgK8UBuSDRhDZTIOQgg",
+  authDomain: "alma-ties.firebaseapp.com",
+  projectId: "alma-ties",
+  storageBucket: "alma-ties.appspot.com",
+  messagingSenderId: "981513285354",
+  appId: "1:981513285354:web:f9ede876f2e9ac0c1af193",
+};
+firebase.initializeApp(firebaseConfig);
